b807719bcd351fddcf73a7f0eb1a3b5da13f355d
Scott Mac McEachern <the_fixer@hotmail.com>
2019-07-20 13:42:27 -0700

    Setup project

---
+++ Data/Data/ClientInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NormalFactory.AppointmentApp.Data
+{
+    /// <summary>
+    /// Contains basic information on the client. Use JsonProperty to ensure property names start with
+    /// lowercase when serialized.
+    /// </summary>
+    public class ClientInfo
+    {
+
+        #region Public Properties
+
+        /// <summary>
+        /// Unique identifier for the user
+        /// </summary>
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// First name of the user
+        /// </summary>
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of the user
+        /// </summary>
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        #endregion
+    }
+}
---
+++ Web/AppointmentApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NormalFactory.AppointmentApp.Web.Models;
+
+namespace NormalFactory.AppointmentApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

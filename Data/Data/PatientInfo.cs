71aec90adc669516f5090a696e2684802a3a077b
Scott Mac McEachern <the_fixer@hotmail.com>
2019-07-20 13:47:31 -0700

    Created patient DTO.

---
+++ Data/Data/PatientInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NormalFactory.AppointmentApp.Data
+{
+    /// <summary>
+    /// Metadata about an individual animal patient. Use JsonProperty to ensure properties start with lowercase when serialized
+    /// </summary>
+    public class PatientInfo
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Unique identifier for the animal
+        /// </summary>
+        [JsonProperty("animalId")]
+        public int AnimalId { get; set; }
+
+        /// <summary>
+        /// Name of the animal
+        /// </summary>
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Type of animal species; potential null
+        /// </summary>
+        [JsonProperty("species")]
+        public string Species { get; set; }
+
+        /// <summary>
+        /// Type of breed of the animal; potentially null
+        /// </summary>
+        [JsonProperty("breed")]
+        public string Breed { get; set; }
+
+        #endregion
+    }
+}
